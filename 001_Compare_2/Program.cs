dc01349da37d40fdf29300f930f7363a4c60efba
Ivan Gr <of_2000@mail.ru>
2022-09-19 20:49:46 +0400

    001 Compare

---
+++ 001_Compare_2/Program.cs
@@ -0,0 +1,9 @@
+﻿//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+using static System.Console;
+
+Clear();
+WriteLine("Input Number #1: ");
+int a = Convert.ToInt32(ReadLine());
+WriteLine("Input Number #2: ");
+int b = Convert.ToInt32(ReadLine());
