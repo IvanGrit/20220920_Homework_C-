3246ccdd68f1c93f417282d87a3199204a34b28d
Ivan Gr <of_2000@mail.ru>
2022-09-20 22:40:04 +0400

    Задача №4

---
+++ 002_Compare_3/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+using static System.Console;
+Clear();
+// Блок ввода чисел 
+WriteLine("Input Number #1: ");
+int a = Convert.ToInt32(ReadLine());
+WriteLine("Input Number #2: ");
+int b = Convert.ToInt32(ReadLine());
+WriteLine("Input Number #3: ");
+int c = Convert.ToInt32(ReadLine());
+// Сравниваем числа и находим максимальное
+int maxNumber = a;
+if(maxNumber < b) maxNumber = b;
+if(maxNumber < c) maxNumber = c;
+Console.WriteLine($"Число {maxNumber} является максимальным из трех введенных");
+
