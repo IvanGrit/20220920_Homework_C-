89d6aacb22159e34f8fbeb969c86da47965e80e3
Ivan Gr <of_2000@mail.ru>
2022-09-20 23:11:14 +0400

    Задача №8

---
+++ 004_1-N/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), 
+// а на выходе показывает все чётные числа от 1 до N.
+
+// Решение №1
+//using static System.Console;
+//Clear();
+//WriteLine("Input Number: ");
+//int a = Convert.ToInt32(ReadLine());
+//int index = 2;
+//Console.WriteLine($"Четными числами в диапзоне от 1 до {a} являются:");
+//while(index <= a)
+//{
+//    Console.Write(index);
+//    Console.Write(" ");
+//    index+=2;
+//}
+
+// Решение №2
+using static System.Console;
+Clear();
+WriteLine("Input Number: ");
+int a = Convert.ToInt32(ReadLine());
+int index = 1;
+Console.WriteLine($"Четными числами в диапзоне от 1 до {a} являются:");
+while(index <= a)
+{
+    if(index % 2 == 0)
+    {
+        Console.Write(index);
+        Console.Write(" ");
+    }
+    index++;
+}
+
