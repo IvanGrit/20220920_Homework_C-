748443eea60e7f2c5e196a12222788f713855446
Ivan Gr <of_2000@mail.ru>
2022-09-20 22:49:09 +0400

    Задача №6

---
+++ 003_Parity/Program.cs
@@ -0,0 +1,9 @@
+﻿// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, 
+// является ли число чётным (делится ли оно на два без остатка).
+
+using static System.Console;
+Clear();
+WriteLine("Input Number: ");
+int a = Convert.ToInt32(ReadLine());
+if(a % 2 == 0) Console.WriteLine($"Число {a} является четным");
+else Console.WriteLine($"Число {a} является нечетным");
